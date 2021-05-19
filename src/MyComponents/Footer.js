11b8cbf8e7e04ec8b266902e2d51a33a8d2fdd48
Rishabh Bansal <rishabhbansal2000@outlook.com>
2021-05-19 19:05:19 +0530

    Create component

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div>
+            <footer id="footer">
+
+                <p id="copy-getmeds">© Copyright 2021 GetMeds</p>
+
+            </footer>
+        </div>
+    )
+}
